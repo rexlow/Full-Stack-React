431135549493accf1d4d5222cef1fe83bb51f667
Rex Low <qiweilow950823@gmail.com>
2017-03-22 00:45:18 +0800

    added ejs, done with boilerplates

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-import config, { nodeEnv } from './config';
+import express from 'express';
+import fs from 'fs';
+
+const server = express();
+
+import config from './config';
+import apiRouter from './api';
+
+server.set('view engine', 'ejs');
+
+//routes
+server.get('/', (req, res) => {
+  res.render('index', {
+    content: 'Hello Express and EJS'
+  });
+});
+
+//middleware
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+
+// server.get('/about.html', (req, res) => {
+//   fs.readFile('./about.html', (err, data) => {
+//     res.send(data.toString());
+//   });
+// });
+
+server.listen(config.port, () => {
+  console.log('Express is listening on port: ', config.port);
+});
